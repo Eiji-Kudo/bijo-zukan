a86c521d06386d59293d0b3c639a18cc708f96a5
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2025-01-03 00:07:23 +0900

    Update ESLint configuration and dependencies for improved linting rules
    - Refactored eslint.config.mjs to include new rules for React and TypeScript, enhancing code quality and consistency.
    - Added specific linting rules for files in the pages directory to manage default exports.
    - Updated package.json and package-lock.json to use the latest version of @eslint/eslintrc and added eslint-plugin-simple-import-sort for better import sorting.
    - Modified the lint script in package.json to automatically fix linting issues during execution.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,31 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "import/no-default-export": "error",
+      "@typescript-eslint/no-unused-vars": ["error", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }]
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/pages/**/*", "**/pages/api/**/*", "**/layout.tsx", "**/page.tsx"],
+    rules: {
+      "import/no-default-export": "off",
+      "import/prefer-default-export": "error",
+    },
+  }
 ];
 
 export default eslintConfig;
